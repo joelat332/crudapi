f63925e7918601d44f968514d391fdd9e549144b
Joel Augustine Tomy <JoelAugustine.Tomy@anko.com>
2024-03-04 16:23:09 +0530

    2nd commit

---
+++ models/productModel.js
@@ -0,0 +1,34 @@
+const mongoose= require('mongoose');
+
+const productSchema= mongoose.Schema(
+    {
+       sampleName:{
+        type : String,
+        required:[true]
+       },
+       sampleNumber:{
+        type : Number,
+        required:[true]
+       },
+       sampleVendor:{
+        id:{
+            type:Number
+        },
+        name:{
+            type:String
+        }        
+        },
+        externalReference:{
+        id:{
+            type:String
+        },
+        source:{
+            type:String
+        }
+        }
+}
+)
+
+const Product=mongoose.model('Product',productSchema);
+
+module.exports=Product;
--- server.js
+++ server.js
@@ -1,9 +1,48 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 const app=express()
+const Product = require('./models/productModel');
+app.use(express.json())
+
 app.get('/',(req, res)=>{
     res.send('hello web')
 })
+app.get('/joel',(req, res)=>{
+    res.send('hi this is joel ')
+})
+app.get('/product',async(req,res)=>{
+    try {
+        const product =await Product.find({});
+        res.status(200).json(product);
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message:error.message})
+    }
+})
 
-app.listen(3000,()=>{
-    console.log("node is running")
+
+app.post('/product',async(req,res)=>{
+    try {
+        const product =await Product.create(req.body)
+        res.status(200).json(product);
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message:error.message})
+    }
+})
+
+
+
+mongoose.
+connect('mongodb://localhost:27017')
+.then(()=>{
+    console.log("connected to DB")
+    app.listen(3000,()=>{
+        console.log("node is running on 3000")
+    })
+    
+}).catch((error)=>{
+    console.log(error)
 })
